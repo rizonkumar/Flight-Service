e83c4b454993061da1f72fc34d45439fff38e05a
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-10-19 11:18:55 +0530

    Fix: Removed unwanted trycatch

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -5,33 +5,26 @@ class CrudRepository {
   constructor(model) {
     this.model = model;
   }
+
   async create(data) {
     const response = await this.model.create(data);
     return response;
   }
 
   async destory(data) {
-    try {
-      const response = await this.model.destory({
-        where: {
-          id: data,
-        },
-      });
-      return response;
-    } catch (error) {
-      logger.error(MESSAGES.ERROR.DATABASE_ERROR);
-      throw error;
-    }
+    const response = await this.model.destory({
+      where: {
+        id: data,
+      },
+    });
+    return response;
   }
+
   async get(data) {
-    try {
-      const response = await this.model.findByPk(data);
-      return response;
-    } catch (error) {
-      logger.error(MESSAGES.ERROR.DATABASE_ERROR);
-      throw error;
-    }
+    const response = await this.model.findByPk(data);
+    return response;
   }
+
   async getAll(data) {
     try {
       const response = await this.model.findAll();
@@ -41,19 +34,15 @@ class CrudRepository {
       throw error;
     }
   }
+
   async update(id, data) {
     // data -> {col:  val, ...}
-    try {
-      const response = await this.model.update(data, {
-        where: {
-          id: id,
-        },
-      });
-      return response;
-    } catch (error) {
-      logger.error(MESSAGES.ERROR.DATABASE_ERROR);
-      throw error;
-    }
+    const response = await this.model.update(data, {
+      where: {
+        id: id,
+      },
+    });
+    return response;
   }
 }
 
