994cba04e87c89f6ebd6682b6040225219f00fcd
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-10-19 17:11:17 +0530

    Feat: Created City Repositoriy

--- src/repositories/airplane-repository.js
+++ src/repositories/airplane-repository.js
@@ -1,6 +1,6 @@
 const CrudRepository = require("./crud-repository");
-
 const { Airplane } = require("../models");
+
 class AirplaneRepository extends CrudRepository {
   constructor() {
     super(Airplane);
