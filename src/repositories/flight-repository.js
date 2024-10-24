65942591c134b82b45394baf38ada9242e3dd4f8
rizon.rahi <rizon.kumar.rahi@gmail.com>
2024-10-24 21:01:07 +0530

    Feat: Added alias and Implemented arrivate aiport Id as well

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -16,15 +16,29 @@ class FlightRepository extends CrudRepository {
         {
           model: Airplane,
           required: true,
+          as: "airplane_detail",
         },
         {
           model: Airport,
           required: true,
+          as: "departure_airport",
           on: {
             col1: Sequelize.where(
               Sequelize.col("Flight.departureAirportId"),
               "=",
-              Sequelize.col("Airport.code")
+              Sequelize.col("departure_airport.code")
+            ),
+          },
+        },
+        {
+          model: Airport,
+          required: true,
+          as: "arrival_airport",
+          on: {
+            col1: Sequelize.where(
+              Sequelize.col("Flight.arrivalAirportId"),
+              "=",
+              Sequelize.col("arrival_airport.code")
             ),
           },
         },
